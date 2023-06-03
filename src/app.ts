312ac274fe7e7fb123cb76e863279e53e3f3c2fe
unknown <maruf.procode@gmail.com>
2023-06-03 11:47:58 +0600

    test setup eslint, prittier, husky, lint-staged

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,18 @@
-import express, { Application } from "express";
-import cors from "cors";
-const app: Application = express();
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
 
 // Application routes
 
-
 // using cors
-app.use(cors());
+app.use(cors())
 
 // parse data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-
-
-
-app.get("/", (req, res) =>{
-    res.send("Hello World");
+app.get('/', (req: Request, res: Response) => {
+  res.send('University Management Authentication Server Running')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 //database connection
 async function connectDB() {
   try {
-    await mongoose.connect(config.mongo_url);
-    console.log(`🛢 Database connection successful`);
-    
+    await mongoose.connect(config.mongo_url)
+    console.log(`🛢 Database connection successful`)
+
     app.listen(config.port, () => {
-      console.log(`Server is  listening on port ${config.port}`);
-    });
+      console.log(`Server is  listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log(`Failed to connect database`, err);
+    console.log(`Failed to connect database`, err)
   }
 }
 
-connectDB();
+connectDB()
