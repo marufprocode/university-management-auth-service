4534c3f3b57647092a7efc878429157de071a0d4
unknown <maruf.procode@gmail.com>
2023-06-03 12:36:46 +0600

    [DONE] user model and interface

---
+++ src/app/modules/users/users.model.ts
@@ -0,0 +1,17 @@
+import { Model, Schema, model } from 'mongoose'
+import { IUser } from './users.interface'
+
+type UserModel = Model<IUser, object>
+
+const userSchema = new Schema<IUser>(
+  {
+    id: { type: String, required: true },
+    role: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true, //this settings will create createdAt and updatedAt automatically by mongoose
+  }
+)
+
+export const User = model<IUser, UserModel>('User', userSchema)
